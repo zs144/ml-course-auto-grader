bbf418cd0e56159c93205143e3f355bd3c3683f7
Zion Sheng <zionsheng@yahoo.com>
2023-06-13 19:26:05 -0400

    Add some comments

--- example/setup.sh
+++ example/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-apt-get install -y python3 python3-pip python3-dev # installs Python and the pip package manager
+# Install Python and the pip package manager.
+apt-get install -y python3 python3-pip python3-dev
 
-pip3 install -r /autograder/source/requirements.txt #
+# Install required packages.
+pip3 install -r /autograder/source/requirements.txt
