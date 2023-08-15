718c99ee28ca86cf140b63c59db33c399d5bd671
Zion Sheng <zionsheng@yahoo.com>
2023-08-15 15:41:58 -0400

    Implement hardcoding detector

--- code/ROC-grader/src/example-question-1/tests.py
+++ code/ROC-grader/src/example-question-1/tests.py
@@ -1,6 +1,7 @@
 import unittest
 from student_utils import *
 
+import copy
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -24,6 +25,9 @@ class TestROCGenerator(unittest.TestCase):
     params = {}
     decision_stats = np.array([])
     ROC_data = pd.DataFrame([])
+    # new data is to check hardcoding
+    new_data = pd.read_csv(hw_folder / 'data/dataSet1.csv', names=['truth', 'x', 'y'])
+    new_data = new_data[['x', 'y', 'truth']]
 
     @classmethod
     def setUpClass(self):
@@ -49,9 +53,19 @@ class TestROCGenerator(unittest.TestCase):
         error_msg = 'Incorrect data type of w (should be np.float64)'
         self.assertIsInstance(b, np.float64, error_msg)
         # Check if there is hardcoding
+        new_train_X = self.__class__.new_data[['x', 'y']].values
+        new_train_Y = self.__class__.new_data['truth']
+        new_params = train_classifier(new_train_X, new_train_Y)
+        new_w = new_params['w']
+        new_b = new_params['b']
+        cmp = ((w == new_w).all() and (b == new_b))
+        error_msg = 'You seem to hardcode the result!'
+        self.assertEqual(cmp, False, error_msg)
+
+        # If passes all:
         print('Good job! No error in step 1!')
 
-    def test_step2(self): # step 1: decision statistics
+    def test_step2(self): # step 2: generating decision statistics
         self.__class__.decision_stats = get_decision_statistics(self.__class__.params, self.__class__.train_X)
         # Check the type of decision stats
         error_msg = 'Incorrect data type of decision statistics'
@@ -60,11 +74,18 @@ class TestROCGenerator(unittest.TestCase):
         error_msg = 'Incorrect shape of decision statistics'
         self.assertEqual(self.__class__.decision_stats.shape, (self.__class__.n_samples,), error_msg)
         # Check if there is hardcoding
+        new_train_X = self.__class__.new_data[['x', 'y']].values
+        new_decision_stats = get_decision_statistics(self.__class__.params, new_train_X)
+        cmp = (self.__class__.decision_stats == new_decision_stats).all()
+        error_msg = 'You seem to hardcode the result!'
+        self.assertEqual(cmp, False, error_msg)
+        # If passes all:
         print('Good job! No error in step 2!')
 
-    def test_step3(self): # step 3:ROC
+    def test_step3(self): # step 3: generating ROC data
         thresholds = np.concatenate((self.__class__.decision_stats, extra))
         self.__class__.ROC_data = get_ROC_data(thresholds, self.__class__.train_y, self.__class__.decision_stats)
+        self.__class__.ROC_data.reset_index(drop=True, inplace=True)
         # Check the type of ROC data
         error_msg = 'Incorrect data type of ROC data table (should be pd.DataFrame)'
         self.assertIsInstance(self.__class__.ROC_data, pd.DataFrame, error_msg)
@@ -81,7 +102,18 @@ class TestROCGenerator(unittest.TestCase):
         is_sorted_pd_and_pfa = (is_pd_asc and is_pfa_asc) or (is_pd_desc and is_pfa_desc)
         error_msg = 'P_d and P_fa are not sorted (both should be monotonic)'
         self.assertEqual(is_sorted_pd_and_pfa, True, error_msg)
-        # Check if ther is hardcoding
+        # Check if there is hardcoding
+        print(self.__class__.ROC_data.head())
+        new_train_X = self.__class__.new_data[['x', 'y']].values
+        new_decision_stats = get_decision_statistics(self.__class__.params, new_train_X)
+        new_thresholds = np.concatenate((new_decision_stats, extra))
+        new_ROC_data = get_ROC_data(new_thresholds, self.__class__.train_y, new_decision_stats)
+        new_ROC_data.reset_index(drop=True, inplace=True)
+        print(new_ROC_data.head())
+        cmp = (self.__class__.ROC_data == new_ROC_data).all().all() # comparing two dfs needs two all()
+        error_msg = 'You seem to hardcode the result!'
+        self.assertEqual(cmp, False, error_msg)
+        # If passes all:
         print('Good job! No error in step 3!')
 
 
